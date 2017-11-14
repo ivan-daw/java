c53d31201ed73e122ad09f647895762a8d17b866
Ivan <32469656+Ivan-J@users.noreply.github.com>
2017-11-14 10:24:35 +0100

    Update NotasDAO.java

--- baseDatos/src/main/java/dao/NotasDAO.java
+++ baseDatos/src/main/java/dao/NotasDAO.java
@@ -1,4 +1,7 @@
 /*
+ex.getStatus "23" fk
+if ex.getMessagee().contains("foreigKey") catch sql inter
+else catch exception
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
